73a314b226ccd876b314b74a52b8015ddfb25f8b
itsnitish22 <nitish.sharma1186@gmail.com>
2021-07-14 23:53:56 +0530

    comments in image function

--- Facial_Recognition.cpp
+++ Facial_Recognition.cpp
@@ -1,31 +1,32 @@
-#include<opencv2/opencv.hpp>
-#include<opencv2/highgui.hpp>
-#include<opencv2/imgproc.hpp>
-#include<opencv2/objdetect.hpp>
-#include<iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
+#include <iostream>
 
 using namespace std;
 using namespace cv;
 
 //HaarCascades Approach
-void face_Detect(Mat img, CascadeClassifier faceCascade)
+void face_Detect(Mat img, CascadeClassifier faceCascade) //function for face detection
 {
-
-	faceCascade.load("haarcascade_frontalface_default.xml");
-	if (faceCascade.empty()) {
+	faceCascade.load("haarcascade_frontalface_default.xml"); //loading xml file which helps to detect the frontal faces
+	if (faceCascade.empty())
+	{
 		cout << "XML file not loaded" << endl;
 	}
 	vector<Rect> faces;
 	faceCascade.detectMultiScale(img, faces, 1.1, 10);
 
 	int count = 0;
-	for (int i = 0; i < faces.size(); ++i) {
+	for (int i = 0; i < faces.size(); ++i)
+	{
 		rectangle(img, faces[i].tl(), faces[i].br(), Scalar(255, 0, 255), 3);
 		count += 1;
 	}
 	string temp = to_string(count);
 
-	putText(img,"No of faces:  "+temp, Point(25, 30), FONT_HERSHEY_COMPLEX_SMALL, 1, LINE_AA);
+	putText(img, "No of faces:  " + temp, Point(25, 30), FONT_HERSHEY_COMPLEX_SMALL, 1, LINE_AA);
 	imshow("Picture_Detection", img);
 	waitKey(0);
 }
@@ -34,8 +35,9 @@ void motion_detect(Mat img1, CascadeClassifier motionCascade)
 {
 	VideoCapture cap(0);
 	motionCascade.load("haarcascade_frontalface_default.xml");
-	if (motionCascade.empty()) {
-		cout << "XML file not loaded"<<endl;
+	if (motionCascade.empty())
+	{
+		cout << "XML file not loaded" << endl;
 	}
 	vector<Rect> faces1;
 
@@ -44,8 +46,9 @@ void motion_detect(Mat img1, CascadeClassifier motionCascade)
 		cap.read(img1);
 		motionCascade.detectMultiScale(img1, faces1, 1.1, 8);
 		int count1 = 0;
-		for (int i = 0; i < faces1.size(); ++i) {
-			rectangle(img1, faces1[i].tl(), faces1[i].br(), Scalar(0, 255,0), 3);
+		for (int i = 0; i < faces1.size(); ++i)
+		{
+			rectangle(img1, faces1[i].tl(), faces1[i].br(), Scalar(0, 255, 0), 3);
 			count1 += 1;
 		}
 		string temp1 = to_string(count1);
@@ -71,18 +74,22 @@ start:
 	cin >> option;
 	switch (option)
 	{
-	case 1: cout << "Enter File name & extension(.jpg/.png)\n";
-			cin >> path;
-			img1 = imread(path);
-			face_Detect(img1, faceCascade);
-			break;
+	case 1:
+		cout << "Enter File name & extension(.jpg/.png)\n";
+		cin >> path;
+		img1 = imread(path);
+		face_Detect(img1, faceCascade);
+		break;
 
-	case 2: cout << "Opening Camera....." << endl;
-			motion_detect(img2, motionCascade);
-			break;
+	case 2:
+		cout << "Opening Camera....." << endl;
+		motion_detect(img2, motionCascade);
+		break;
 
-	default: cout << "Invalid Input" << "\n";
-			goto start;
+	default:
+		cout << "Invalid Input"
+			 << "\n";
+		goto start;
 	}
 	return 0;
 }
