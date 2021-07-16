75da9b5530988dadb3fa89ced71020f67e38e589
itsnitish22 <nitish.sharma1186@gmail.com>
2021-07-16 13:53:50 +0530

    did minor changes in code formatted the comments

--- Facial_Recognition.cpp
+++ Facial_Recognition.cpp
@@ -11,31 +11,31 @@ using namespace cv;
 void face_Detect(Mat img, CascadeClassifier faceCascade) //function for face detection in image
 {
 	faceCascade.load("haarcascade_frontalface_default.xml"); //loading xml file for detecting frontal faces
-	if (faceCascade.empty())								 // if file detected as empty
+	if (faceCascade.empty())	// if file detected as empty
 	{
 		cout << "XML file not loaded" << endl;
 	}
-	vector<Rect> faces;								   //creating a vector 'rect' that is an instance of SVG shape and draws rectangles at defined positions
+	vector<Rect> faces;	//creating a vector 'rect' that is an instance of SVG shape and draws rectangles at defined positions
 	faceCascade.detectMultiScale(img, faces, 1.1, 10); //function detecting objects of different sizes in the input image and returing vector of rectangles
 
 	int face_count = 0; //initaizing the face count to 0
 	for (int i = 0; i < faces.size(); ++i)
 	{
 		rectangle(img, faces[i].tl(), faces[i].br(), Scalar(255, 0, 255), 3); //making rectangles on detected faces
-		face_count += 1;													  //incrementing face_count by 1 each time a face is found
+		face_count += 1;	//incrementing face_count by 1 each time a face is found
 	}
 	string temp = to_string(face_count); //storing count of faces as string in temporary variable
 
 	putText(img, "No of faces:  " + temp, Point(25, 30), FONT_HERSHEY_COMPLEX_SMALL, 1, LINE_AA); //printing text at top right corner showing number of faces
-	imshow("Picture_Detection", img);															  //detecting faces in a new window named	'Picture_Detection'
-	waitKey(0);																					  //displaying Picture_Detection window infinitely until any keypress
+	imshow("Picture_Detection", img);	//detecting faces in a new window named	'Picture_Detection'
+	waitKey(0);	//displaying Picture_Detection window infinitely until any keypress
 }
 
 void motion_detect(Mat img1, CascadeClassifier motionCascade) //function for face detection in video
 {
-	VideoCapture cap(0);									   //function for capturing video
+	VideoCapture cap(0);	//function for capturing video
 	motionCascade.load("haarcascade_frontalface_default.xml"); //loading xml file for motion detection in video
-	if (motionCascade.empty())								   //if file found empty
+	if (motionCascade.empty())	//if file found empty
 	{
 		cout << "XML file not loaded" << endl;
 	}
@@ -43,18 +43,18 @@ void motion_detect(Mat img1, CascadeClassifier motionCascade) //function for fac
 
 	while (true) //infinite loop
 	{
-		cap.read(img1);										  //reading frame of detected video
+		cap.read(img1);	//reading frame of detected video
 		motionCascade.detectMultiScale(img1, faces1, 1.1, 8); //detecting objects of different sizes
-		int face_count2 = 0;								  //face count in video initialized as 0
+		int face_count2 = 0;	//face count in video initialized as 0
 		for (int i = 0; i < faces1.size(); ++i)
 		{
 			rectangle(img1, faces1[i].tl(), faces1[i].br(), Scalar(0, 255, 0), 3); //making rectangles on faces
-			face_count2 += 1;													   //incrementing face count by 1
+			face_count2 += 1;	//incrementing face count by 1
 		}
-		string temp1 = to_string(face_count2);															//coverting face_count2 to string
+		string temp1 = to_string(face_count2);	//coverting face_count2 to string
 		putText(img1, "No of faces:  " + temp1, Point(30, 45), FONT_HERSHEY_COMPLEX_SMALL, 1, LINE_AA); //printing text at top left corner with face count
-		imshow("Motion_Detection", img1);																//new window for video
-		waitKey(1);																						//displaying window infinitely
+		imshow("Motion_Detection", img1);	//new window for video
+		waitKey(1);	//displaying window infinitely
 	}
 }
 
@@ -87,8 +87,7 @@ start:
 		break;
 
 	default:
-		cout << "Invalid Input"
-			 << "\n";
+		cout << "Invalid Input\n";
 		goto start;
 	}
 	return 0;
